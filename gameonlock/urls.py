ada97d4e1e4c6288322cadd9565fe069174d1b41
naggafin <nevincoutu@gmail.com>
2025-03-02 19:47:51 -0500

    bug fixes

--- gameonlock/urls.py
+++ gameonlock/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
+from django.views.i18n import set_language
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -42,6 +43,7 @@ internatonalized_patterns = i18n_patterns(
 urlpatterns = [
 	path("admin/", admin.site.urls),
 	path("csp-report/", views.csp_report_view, name="csp_report"),
+	path("i18n/set_language/", set_language, name="set_language"),
 	path("cms/", include(wagtailadmin_urls)),
 	path("documents/", include(wagtaildocs_urls)),
 ] + internatonalized_patterns
